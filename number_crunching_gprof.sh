772eb1b784878dc582087442dd7e7509a6c8e27d
Adrian <adrian6476@outlook.com>
2024-04-15 02:40:33 +0100

    Task 1.2

---
+++ number_crunching_gprof.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# compile
+g++ -pg -o number_crunching number_crunching.cpp
+
+# run the program and collect data
+echo "Performance analysis with gprof:" > number_crunching_gprof.out
+for k in {1..10}
+do
+    N=$((k * 10000))
+    echo "Running for N = $N" >> number_crunching_gprof.out
+    ./number_crunching $N
+    gprof number_crunching gmon.out >> number_crunching_gprof.out
+done
