838c818214bdd5ab0d1891a91f7e0e0eb94b9a71
Adrian <adrian6476@outlook.com>
2024-04-15 03:24:46 +0100

    Simplify task 1.2  output

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -10,5 +10,5 @@ do
     N=$((k * 10000))
     echo "Running for N = $N" >> number_crunching_gprof.out
     ./number_crunching $N
-    gprof number_crunching gmon.out >> number_crunching_gprof.out
+    gprof number_crunching gmon.out --brief >> number_crunching_gprof.out
 done
