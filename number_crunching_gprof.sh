966c6eb3c875b078ccbc5a545fdf42d352ae4f68
WANG <dgwz78@login2.ham8.dur.ac.uk>
2024-04-15 04:03:52 +0100

    task 1.2 output

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -5,14 +5,10 @@ g++ -pg -o number_crunching number_crunching.cpp
 
 # run the program and collect data
 echo "Performance analysis with gprof:" > number_crunching_gprof.out
-
 for k in {1..10}
 do
     N=$((k * 10000))
     echo "Running for N = $N" >> number_crunching_gprof.out
-
     ./number_crunching $N
-
-    gprof number_crunching gmon.out | grep -A1 "Flat profile:" | grep -v "Flat profile:" | grep -v "Each sample counts" | head -n 10 >> number_crunching_gprof.out
-    echo "--------------------------------" >> number_crunching_gprof.out
+    gprof number_crunching gmon.out --brief --no-graph>> number_crunching_gprof.out
 done
