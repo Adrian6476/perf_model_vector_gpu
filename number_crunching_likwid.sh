ef16b98dc410d0f33790364e53ea2a0b491f1404
Adrian <adrian6476@outlook.com>
2024-04-23 15:38:59 +0100

    likwid.sh

--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -1,5 +1,5 @@
-
 #!/bin/bash
+
 echo "Before setting, GROUPS: $GROUPS"
 unset GROUPS
 echo "After unsetting, GROUPS: $GROUPS"
@@ -7,11 +7,16 @@ echo "After unsetting, GROUPS: $GROUPS"
 # Input size (N)
 N=10000
 
-# Performance groups to measure
+# Ensure the executable permission is set
+chmod +x number_crunching_likwid
+
+# New variable name to avoid any conflicts
 PERF_GROUPS=("FLOPS_DP" "L3" "CACHE" "DATA" "MEM")
+echo "Reset PERF_GROUPS: ${PERF_GROUPS[@]}"
 
 # Run the instrumented code with likwid-perfctr for each group
 for GROUP in "${PERF_GROUPS[@]}"
 do
+    echo "Running group: $GROUP"
     likwid-perfctr -C 0 -g $GROUP -m ./number_crunching_likwid $N > "number_crunching_likwid_$GROUP.out"
 done
