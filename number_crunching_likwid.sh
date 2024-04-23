792d626e5e8f606cfefe6ed7f3fab9983e0d06a1
Adrian <adrian6476@outlook.com>
2024-04-23 14:53:46 +0100

    update likwid sh

--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# The number of threads
-export OMP_NUM_THREADS=1
+# The performance counters or counter groups to measure
+COUNTERS="FLOPS_DP:PMC0,FLOPS_AVX:PMC1,MEM_BW:PMC2,L3:PMC3"
+
+# Input size (N)
+N=10000
+
+# Compile the instrumented code
+g++ -DLIKWID_PERFMON -o number_crunching_likwid number_crunching_likwid.cpp -llikwid
 
 # Run the instrumented code with likwid-perfctr
-likwid-perfctr -C 0 -g FLOPS_DP -m ./number_crunching_likwid 10000 > number_crunching_likwid.out
+$likwid-perfctr -C 0 -g $COUNTERS -m ./number_crunching_likwid $N
+
+# Save the output to a file
+$likwid-perfctr -C 0 -g $COUNTERS -m ./number_crunching_likwid $N > number_crunching_likwid.out
+
+# Clean up the executable
+rm number_crunching_likwid
